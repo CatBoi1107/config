f83f16157dcf8f37f434556cef21ffbc38f3ffcd
CatBoi1107 <therealjohmsith1256@gmail.com>
2026-04-14 18:11:35 +0700

    Yazi QoL plugins

--- commands/screenshot.sh
+++ commands/screenshot.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-hyprshot -m region --clipboard-only --freeze
+# hyprshot -m region --clipboard-only --freeze
+hyprshot -m region -o ~/ThePreservedLexicon/Images/Screenshots/ -f ss_$(date +%Y%m%d_%H).png --freeze
