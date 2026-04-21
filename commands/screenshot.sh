98fb7d10c1c7eeddb21e30fb243488daba272b4e
CatBoi1107 <therealjohmsith1256@gmail.com>
2026-04-21 21:17:42 +0700

    I forgor

--- commands/screenshot.sh
+++ commands/screenshot.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # hyprshot -m region --clipboard-only --freeze
-hyprshot -m region -o ~/ThePreservedLexicon/Images/Screenshots/ -f ss_$(date +%Y%m%d_%H).png --freeze
+# hyprshot -m region -o ~/ThePreservedLexicon/Images/Screenshots/ -f $(date +%Y%m%d_%H).png --freeze
+hyprshot -m region -o ~/ThePreservedLexicon/Images/Screenshots/ -f $(date +%d%m%y_%H).png --freeze
